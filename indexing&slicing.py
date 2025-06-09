7805fa64f5e61416a300a7b4900053832f1e369e
Knights24 <vivekvishwakarma21@outlook.com>
2025-06-09 20:49:44 +0530

    Day 1

---
+++ indexing&slicing.py
@@ -0,0 +1,12 @@
+str1 = 'hello'
+str2 = 'vivek' 
+
+str = str1 + str2 #concatenation
+print(str)
+
+print(str[6]) #indexing
+print(str[2:8]) #slicing
+
+
+str = 'Apple'
+print(str[-3:-1]) #negative indexing
