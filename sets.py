06121db6aae1800151bea5e9c38e7a14f398de50
Knights24 <vivekvishwakarma21@outlook.com>
2025-06-11 08:04:48 +0530

    Sets

---
+++ sets.py
@@ -0,0 +1,5 @@
+#build in datatype in python like lists and tupple sets are mutable and not allow to repeat the valus
+#Syntax like list
+#Sets are mutable but elemets are not mutable
+Set = {"VIvek",20}
+print(type(Set))
---
+++ setvsarrayvstupple.py
@@ -0,0 +1,7 @@
+Array = [1,2,3] #list
+tupple = (1,2,3)
+set = {1,2,3}
+
+print(type(Array))
+print(type(tupple))
+print(type(set))
