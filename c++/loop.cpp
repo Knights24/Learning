d39ecc5039e2ded493f6bd9c4aa6f29c70622f70
Knights24 <vivekvishwakarma21@outlook.com>
2025-06-14 13:09:02 +0530

    loop

--- c++/loop.cpp
+++ c++/loop.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 using namespace std;
-
-int main(){
-    int vari = 1; 
-    while(vari <= 15){
-        cout << vari << endl ; 
-        vari++;
+int main() {
+    int n = 10;
+    int sum = 0;
+    for(int i = 0; i <=n; i++){
+        if(i%2 == 0 ){
+            sum += i;
+            cout << sum <<endl;
+        }
     }
-    
+
     return 0;
 }
+  
