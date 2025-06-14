b560ed091a9f7dfc6839bd685cc7555f3a3f9354
Knights24 <vivekvishwakarma21@outlook.com>
2025-06-14 12:51:19 +0530

    createloop

---
+++ c++/loop.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int vari = 1; 
+    while(vari <= 15){
+        cout << vari << endl ; 
+        vari++;
+    }
+    
+    return 0;
+}
