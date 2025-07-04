92d6768a3241e72750c517ed6defb62d1912541f
Vivek Vishwakarma <vivekvishwakarma21@outlook.com>
2025-07-05 00:54:38 +0530

    Update sumOfTwo.cpp

--- c++/sumOfTwo.cpp
+++ c++/sumOfTwo.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main(){
     int a, b;
-    cout << "Enter two numbers: ";
+    cout << "Enter two";
     cin >> a >> b;
     int sum = a + b;        
     cout << "Sum of " << a << " and " << b << " is: " << sum << endl;
